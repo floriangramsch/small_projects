10f1a0bee9c9c8093925db5e4d5a6111c0d8b53e
Florian Gramsch <FloriGram@gmail.com>
2021-04-20 23:10:53 +0200

    dinge

---
+++ kivy/crimp.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+
+class Crimp(Widget):
+    def build(self):
+        hbox = BoxLayout(orientation="horizontal")
+
+        # self.label = Label(text="crimpen")
+        # self.label2 = Label(text="mehr crimpen")
+        # self.label3 = Label(text="noch mehr crimpen")
+        # self.add_widget(self.label)
+        # self.add_widget(self.label2)
+        # self.add_widget(self.label3)
+    pass
+        # return self
