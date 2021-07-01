07cb0bd8c2e63f43b0466131dd3556f7f1b64f1e
Florian Gramsch <FloriGram@gmail.com>
2021-07-01 13:58:56 +0200

    better system

--- sql_points/main.py
+++ sql_points/main.py
@@ -16,80 +16,134 @@ class MainWindow(qw.QDialog):
         super(MainWindow, self).__init__(parent)
         self.init_db()
 
-        # self.figure, self.axis = plt.subplots()
-        # self.canvas = FigureCanvas(self.figure)
-        # self.toolbar = NavigationToolbar(self.canvas, self)
-
         fbox = qw.QFormLayout()
-        fbox.addRow("Statistik:", qw.QLabel(str(self.statistik_grade[0])))
-        fbox.addRow("Datenbanken:", qw.QLabel(str(self.datenbanken_grade[0])))
-        fbox.addRow("Programmiersprachen:", qw.QLabel(str(self.programmiersprachen_grade[0])))
-        fbox.addRow("Mathe für Physiker 2:", qw.QLabel(str(self.mfp2_grade[0])))
-        fbox.addRow("Mathematische Modellierung 1:", qw.QLabel(str(self.mmr1_grade[0])))
-        fbox.addRow("Test:", qw.QLabel(str(self.test_grade[0])))
+        self.statistik_mean = qw.QLabel(str(self.statistik_grade[0]))
+        fbox.addRow("Statistik:", self.statistik_mean)
 
-        points = qw.QLineEdit()
-        points.setValidator(qg.QIntValidator())
+        self.db_mean = qw.QLabel(str(self.datenbanken_grade[0]))
+        fbox.addRow("Datenbanken:", self.db_mean)
+
+        self.ps_mean = qw.QLabel(str(self.programmiersprachen_grade[0]))
+        fbox.addRow("Programmiersprachen:", self.ps_mean)
+
+        self.mfp2_mean = qw.QLabel(str(self.mfp2_grade[0]))
+        fbox.addRow("Mathe für Physiker 2:", self.mfp2_mean)
+
+        self.mmr1_mean = qw.QLabel(str(self.mmr1_grade[0]))
+        fbox.addRow("Mathematische Modellierung 1:", self.mmr1_mean)
+
+        self.test_mean = qw.QLabel(str(self.test_grade[0]))
+        fbox.addRow("Test:", self.test_mean)
+
+        self.points = qw.QLineEdit()
+        self.points.setValidator(qg.QDoubleValidator())
         points_fbox = qw.QFormLayout()
-        points_fbox.addRow("Punkte:", points)
-        total = qw.QLineEdit()
-        total.setValidator(qg.QIntValidator())
+        points_fbox.addRow("Punkte:", self.points)
+        self.total = qw.QLineEdit()
+        self.total.setValidator(qg.QDoubleValidator())
         total_fbox = qw.QFormLayout()
-        total_fbox.addRow("Gesamt:", total)
+        total_fbox.addRow("Gesamt:", self.total)
         hbox2 = qw.QHBoxLayout()
         hbox2.addLayout(points_fbox)
         hbox2.addLayout(total_fbox)
         
-        combo = qw.QComboBox()
-        combo.addItems(["Test", "Statistik", "Datenbanken", "Programmiersprachen", "MfP2", "MMR1"])
+        self.combo = qw.QComboBox()
+        self.combo.addItems(["Test", "Statistik", "Datenbanken", "Programmiersprachen", "MfP2", "MMR1"])
         btn = qw.QPushButton("Update Points")
-        btn_insert = f"INSERT INTO {combo.currentText()} (points, total) VALUES (%s, %s)"
-        btn.clicked.connect(lambda: [self.mycursor.execute(btn_insert, (points.text(), total.text())), self.mydb.commit(), print("Value Updated!")])
+        btn.clicked.connect(lambda: [self.update()])
+
+        btn_test_zero = qw.QPushButton("Test Zero")
+        btn_test_zero.clicked.connect(lambda: self.test_zero())
+
         hbox1 = qw.QHBoxLayout()
         hbox1.addWidget(btn)
-        hbox1.addWidget(combo)
+        hbox1.addWidget(self.combo)
 
         layout = qw.QVBoxLayout()
         layout.addLayout(fbox)
         layout.addLayout(hbox2)
         layout.addLayout(hbox1)
+        layout.addWidget(btn_test_zero)
         self.setLayout(layout)
 
+    def update(self):
+        btn_insert = f"INSERT INTO {self.combo.currentText()} (points, total) VALUES (%s, %s)"
+        self.mycursor.execute(btn_insert, (self.points.text(), self.total.text()))
+        self.mydb.commit()
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Statistik;")
+        self.statistik_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from MMR1;")
+        self.mmr1_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from MfP2;")
+        self.mfp2_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Programmiersprachen;")
+        self.ps_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Datenbanken;")
+        self.db_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Test;")
+        self.test_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        print(f"{self.combo.currentText()} Value Updated!")
+
+    def test_zero(self):
+        btn_insert = f"INSERT INTO {self.combo.currentText()} (points, total) VALUES (%s, %s)"
+        self.mycursor.execute(btn_insert, (0, self.total.text()))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Statistik;")
+        self.statistik_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from MMR1;")
+        self.mmr1_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from MfP2;")
+        self.mfp2_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Programmiersprachen;")
+        self.ps_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Datenbanken;")
+        self.db_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Test;")
+        self.test_mean.setText(str(self.mycursor.fetchone()[0]))
+
+        print(f"{self.combo.currentText()} Value Updated!")
+
     def init_db(self):
+        # self.mydb = mysql.connector.connect(
+        #     host="localhost",
+        #     user="root",
+        #     password="1234",
+        #     database="points"
+        #     )
         self.mydb = mysql.connector.connect(
             host="localhost",
-            user="root",
-            password="1234",
+            user="flo",
+            password="2406",
             database="points"
             )
         self.mycursor = self.mydb.cursor()
 
         self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Statistik;")
         self.statistik_grade = self.mycursor.fetchone()
-        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from mmr1;")
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from MMR1;")
         self.mmr1_grade = self.mycursor.fetchone()
-        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from mfp2;")
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from MfP2;")
         self.mfp2_grade = self.mycursor.fetchone()
-        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from programmiersprachen;")
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Programmiersprachen;")
         self.programmiersprachen_grade = self.mycursor.fetchone()
-        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from datenbanken;")
+        self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Datenbanken;")
         self.datenbanken_grade = self.mycursor.fetchone()
         self.mycursor.execute("SELECT round((sum(points)/sum(total))*100, 2) as Average from Test;")
         self.test_grade = self.mycursor.fetchone()
 
-    def plot(self):
-        pass
-
-        # Zeichnen und Anzeige
-        # self.axis.plot(x, y, label='Funktion')
-        # self.axis.legend()
-
-        # Achtung: keine plt.show!
-        # (Neu-)Zeichnen des Canva
-        # self.canvas.draw()
-
-if __name__ == '__main__':
-    app = qw.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
-    sys.exit(app.exec_())
+app = qw.QApplication(sys.argv)
+main = MainWindow()
+main.show()
+sys.exit(app.exec_())
